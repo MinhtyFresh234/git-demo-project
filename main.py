ee0cd4d1cfd95fb75d2e218d6b8ea65f9a302c21
MinhtyFresh234 <mtran7131@gmail.com>
2024-11-03 11:44:43 -0700

    Add logging to greet function

--- main.py
+++ main.py
@@ -1,6 +1,13 @@
+import logging
+logging.basicConfig(level=logging.INFO)
+
 print('Hello, World\!')
 
 def greet(name):
     return f"Hello, {name}!"
 
 print(greet("World"))
+
+def greet(name):
+    logging.info(f"Greeting {name}")
+    return f"Hello, {name}!"
